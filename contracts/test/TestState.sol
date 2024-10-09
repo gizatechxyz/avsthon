21b97c253b706385c042e4b60252c59c9ff6f30f
chalex.eth <43524913+chalex-eth@users.noreply.github.com>
2024-10-09 21:08:06 +0200

    Add ClientRegistry and tests

--- contracts/test/TestState.sol
+++ contracts/test/TestState.sol
@@ -3,28 +3,30 @@ pragma solidity 0.8.26;
 
 import {Test} from "forge-std/Test.sol";
 import {TaskRegistry} from "src/TaskRegistry.sol";
+import {ClientAppRegistry} from "src/ClientAppRegistry.sol";
 
 contract TestState is Test {
     address public owner = makeAddr("owner");
     address public user = makeAddr("user");
     address public aggregatorNode = makeAddr("aggregatorNode");
-    address public clientAppRegistry = makeAddr("clientAppRegistry");
 
     TaskRegistry public taskRegistry;
+    ClientAppRegistry public clientAppRegistry;
 
     function setUp() public virtual {
         vm.startPrank(owner);
-        taskRegistry = new TaskRegistry(owner, aggregatorNode, clientAppRegistry);
+        clientAppRegistry = new ClientAppRegistry(owner);
+        taskRegistry = new TaskRegistry(owner, aggregatorNode, address(clientAppRegistry));
         vm.stopPrank();
         vm.label(owner, "owner");
         vm.label(user, "user");
         vm.label(aggregatorNode, "aggregatorNode");
-        vm.label(clientAppRegistry, "clientAppRegistry");
+        vm.label(address(clientAppRegistry), "clientAppRegistry");
     }
 
     function test_initialState() public view {
         assertEq(taskRegistry.owner(), owner);
         assertEq(taskRegistry.aggregatorNode(), aggregatorNode);
-        assertEq(taskRegistry.clientAppRegistry(), clientAppRegistry);
+        assertEq(taskRegistry.clientAppRegistry(), address(clientAppRegistry));
     }
 }
