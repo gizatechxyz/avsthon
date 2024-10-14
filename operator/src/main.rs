19884b85f27cf13249ef702380cdd4f5c364adc5
chalex.eth <43524913+chalex-eth@users.noreply.github.com>
2024-10-14 14:19:31 +0200

    Implement event listening and queue system for operator

---
+++ operator/src/main.rs
@@ -0,0 +1,28 @@
+use eyre::Result;
+use operator::Operator;
+use time::macros::format_description;
+use tracing_subscriber::fmt;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Initialize the tracing subscriber with custom filter and format
+    let format = fmt::format()
+        .with_level(true)
+        .with_target(true)
+        .with_thread_ids(false)
+        .with_thread_names(false)
+        .with_timer(fmt::time::UtcTime::new(format_description!(
+            "[year]-[month]-[day] [hour]:[minute]:[second]"
+        )))
+        .compact()
+        .with_source_location(false)
+        .with_ansi(true);
+
+    tracing_subscriber::fmt()
+        .event_format(format)
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
+    let operator = Operator::new().await?;
+    operator.run().await
+}
