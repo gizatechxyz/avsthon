9c7f7a63a48b2d8bfe61871358464bb9ed902199
chalex.eth <43524913+chalex-eth@users.noreply.github.com>
2024-10-18 12:08:03 +0200

    Update deployment script on holesky fork

--- Makefile
+++ Makefile
@@ -4,14 +4,20 @@
 help:
 	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-RPC_URL=http://localhost:8545
+HOLESKY_RPC_URL=https://eth-holesky.g.alchemy.com/v2/8lbq3evplhjE7rP48rxeMXcpDNTGz0Hf
+ANVIL_RPC_URL=http://localhost:8545
 DEPLOYER_PK=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-TASK_REGISTRY_ADDRESS=0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+GIZA_AVS_ADDRESS=0x6Da3D07a6BF01F02fB41c02984a49B5d9Aa6ea92
+TASK_REGISTRY_ADDRESS=0xa8d297D643a11cE83b432e87eEBce6bee0fd2bAb
+CLIENT_APP_REGISTRY_ADDRESS=0xb4e9A5BC64DC07f890367F72941403EEd7faDCbB
 TASK_ID=0xc86aab04e8ef18a63006f43fa41a2a0150bae3dbe276d581fa8b5cde0ccbc966
 
 -----------------------------: ##
 
 ___CONTRACTS___: ##
+
+anvil: ## starts anvil
+	anvil --ipc --fork-url $(HOLESKY_RPC_URL)
 
 build-contracts: ## builds all contracts
 	cd contracts && forge build
@@ -20,9 +26,8 @@ test-contracts: ## tests all contracts
 	cd contracts && forge test
 
 deploy-contracts: ## deploy contracts  (you need to run anvil first in a separate terminal and the contract deployed)
-	cd contracts && forge script script/DeployTaskAndAppRegistry.s.sol --rpc-url $(RPC_URL) --broadcast --private-key $(DEPLOYER_PK)
+	cd contracts && forge script script/DeployContracts.s.sol --rpc-url $(ANVIL_RPC_URL) --broadcast --private-key $(DEPLOYER_PK)
 
 __TASKS__: ##
-
 create-task: ## create a task (you need to run anvil first in a separate terminal and the contract deployed)
-	cast send $(TASK_REGISTRY_ADDRESS)  "createTask(bytes32)" $(TASK_ID) --private-key $(DEPLOYER_PK) --rpc-url $(RPC_URL)
+	cast send $(TASK_REGISTRY_ADDRESS)  "createTask(bytes32)" $(TASK_ID) --private-key $(DEPLOYER_PK) --rpc-url $(ANVIL_RPC_URL)
--- contracts/script/DeployContracts.s.sol
+++ contracts/script/DeployContracts.s.sol
@@ -5,10 +5,12 @@ import {Script, console2} from "forge-std/Script.sol";
 import {Constants} from "./Constants.sol";
 import {TaskRegistry} from "../src/TaskRegistry.sol";
 import {ClientAppRegistry, ClientAppMetadata} from "../src/ClientAppRegistry.sol";
+import {GizaAvs} from "../src/GizaAvs.sol";
 
-contract DeployTaskAndAppRegistry is Script, Constants {
+contract DeployContracts is Script, Constants {
     TaskRegistry public taskRegistry;
     ClientAppRegistry public clientAppRegistry;
+    GizaAvs public gizaAvs;
 
     function run() public {
         console2.log("Deploying contracts...");
@@ -16,22 +18,25 @@ contract DeployTaskAndAppRegistry is Script, Constants {
 
         vm.startBroadcast();
 
+        // Deploy contracts
         clientAppRegistry = new ClientAppRegistry(msg.sender);
-        console2.log("ClientAppRegistry deployed at %s", address(clientAppRegistry));
-
         taskRegistry = new TaskRegistry(msg.sender, AGGREGATOR_NODE, address(clientAppRegistry));
-        console2.log("TaskRegistry deployed at %s", address(taskRegistry));
+        gizaAvs = new GizaAvs(msg.sender, HOLESKY_EIGENLAYER_AVS_DIRECTORY, address(clientAppRegistry));
 
+        console2.log("ClientAppRegistry deployed at %s", address(clientAppRegistry));
+        console2.log("TaskRegistry deployed at %s", address(taskRegistry));
+        console2.log("GizaAvs deployed at %s", address(gizaAvs));
+
+        // Register tasks
         bytes32 clientAppId = keccak256("ethereum-block-number");
         console2.logBytes32(clientAppId);
-
         clientAppRegistry.registerClientApp(
             clientAppId,
             ClientAppMetadata({
                 name: "Ethereum Block Number",
                 description: "This task returns the current block number of the Ethereum network.",
                 logoUrl: "",
-                dockerUrl: ""
+                dockerUrl: "https://hub.docker.com/layers/chalex443/ethereum-block-number/latest/images/sha256:b0aba1ccde00369af2432a13e3412a9ac037a521c25e94da91c8c7b5f30b6544"
             })
         );
 
